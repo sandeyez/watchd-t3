8932d83a412eafc135f6e3484680c975d3e25a30
sandeyez <s.a.f.vanwerkhooven@students.uu.nl>
2024-04-23 18:05:44 +0200

    Add shadcn/ui and configure prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,13 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 4,
 };
 
 export default config;
