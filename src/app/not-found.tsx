3821855e50795f964c429c2677f3cc4df7014db3
sandeyez <s.a.f.vanwerkhooven@students.uu.nl>
2024-04-30 12:05:06 +0200

    Style changes for not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,8 +3,8 @@ import Button from "./_components/Button/Button";
 
 export default async function NotFoundPage() {
     return (
-        <div className="relative flex h-full w-full flex-col items-center justify-center gap-8">
-            <div className="fixed left-0 top-[72px] -z-10 h-full w-full bg-gradient-to-b from-tertiary to-primary"></div>
+        <div className="relative flex h-full w-full flex-col items-center justify-center gap-8 p-8">
+            <div className="fixed left-0 top-[72px] -z-10 h-full w-full bg-gradient-to-b from-secondary to-tertiary"></div>
             <div className="flex max-w-lg flex-col items-center gap-2 text-center">
                 <span className="gradient-text text-4xl font-bold">
                     Oops...
