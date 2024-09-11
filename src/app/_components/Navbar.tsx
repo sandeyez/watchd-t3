5359b88f9ea2f313fb647ca6211c2683b75b006a
sandeyez <s.a.f.vanwerkhooven@students.uu.nl>
2024-09-11 19:08:36 +0200

    Make mobile nav fixed

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -82,88 +82,92 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="flex min-h-[72px] items-center justify-between gap-4 bg-primary px-6 py-4 md:grid md:grid-flow-col">
-            <Link
-                href="/"
-                className="flex h-fit items-center justify-center md:justify-start"
-            >
-                <Image
-                    src="/logo.svg"
-                    width={120}
-                    height={24}
-                    alt="Watchd logo"
-                    className="h-6"
-                />
-            </Link>
-            <div className="hidden flex-grow flex-col items-center justify-start gap-8 pt-8 md:flex md:flex-row md:justify-center md:gap-12 md:pt-0">
-                {navItems.map((item, index) => (
-                    <NavItem key={index} {...item} />
-                ))}
-            </div>
-            <div className="hidden items-center justify-end md:flex">
-                {status === "authenticated" ? (
-                    <Link href="/profile" className="text-white">
-                        <PersonAvatar
-                            src={data.user.image}
-                            altText="Profile picture"
-                            variant="small"
-                            withBorder
-                        />
-                    </Link>
-                ) : (
-                    <Link href="/login" className="text-white">
-                        Login
-                    </Link>
-                )}
-            </div>
+        <>
+            <div className="block min-h-[72px] w-screen md:hidden"></div>
+            <nav className="fixed left-0 top-0 z-20 flex min-h-[72px] w-screen items-center justify-between gap-4 bg-primary px-6 py-4 md:static md:grid md:grid-flow-col">
+                {/* Skeleton to use for screens smaller than md */}
+                <Link
+                    href="/"
+                    className="flex h-fit items-center justify-center md:justify-start"
+                >
+                    <Image
+                        src="/logo.svg"
+                        width={120}
+                        height={24}
+                        alt="Watchd logo"
+                        className="h-6"
+                    />
+                </Link>
+                <div className="hidden flex-grow flex-col items-center justify-start gap-8 pt-8 md:flex md:flex-row md:justify-center md:gap-12 md:pt-0">
+                    {navItems.map((item, index) => (
+                        <NavItem key={index} {...item} />
+                    ))}
+                </div>
+                <div className="hidden items-center justify-end md:flex">
+                    {status === "authenticated" ? (
+                        <Link href="/profile" className="text-white">
+                            <PersonAvatar
+                                src={data.user.image}
+                                altText="Profile picture"
+                                variant="small"
+                                withBorder
+                            />
+                        </Link>
+                    ) : (
+                        <Link href="/login" className="text-white">
+                            Login
+                        </Link>
+                    )}
+                </div>
 
-            <div className="block md:hidden">
-                <FontAwesomeIcon
-                    icon={isMenuOpen ? faClose : faBars}
-                    className="h-6 w-6 cursor-pointer text-white"
-                    onClick={toggleMenu}
-                />
-            </div>
-            <AnimatePresence initial={false} mode="wait">
-                {isMenuOpen && (
-                    <>
-                        <motion.div
-                            className="fixed bottom-0 left-0 z-40 h-[calc(100dvh-72px)] w-screen"
-                            initial={{
-                                backdropFilter: "blur(0px)",
-                            }}
-                            animate={{
-                                backdropFilter: "blur(12px)",
-                            }}
-                            exit={{
-                                backdropFilter: "blur(0px)",
-                            }}
-                            onClick={toggleMenu}
-                        />
-                        <motion.div
-                            className="fixed bottom-0 z-50 h-[calc(100dvh-72px)] w-[50vw] min-w-72 max-w-[100vw] bg-secondary"
-                            initial={{
-                                right: "-100%",
-                            }}
-                            animate={{
-                                right: isMenuOpen ? "0%" : "-100%",
-                            }}
-                            exit={{
-                                right: "-100%",
-                            }}
-                        >
-                            <div className="flex h-full w-full">
-                                <div className="h-full w-[2px] bg-gradient-to-b from-gradientPink to-gradientBlue" />
-                                <div className="flex flex-col gap-8 p-8">
-                                    {navItems.map((item, index) => (
-                                        <NavItem key={index} {...item} />
-                                    ))}
+                <div className="block md:hidden">
+                    <FontAwesomeIcon
+                        icon={isMenuOpen ? faClose : faBars}
+                        className="h-6 w-6 cursor-pointer text-white"
+                        onClick={toggleMenu}
+                    />
+                </div>
+                <AnimatePresence initial={false} mode="wait">
+                    {isMenuOpen && (
+                        <>
+                            <motion.div
+                                className="fixed bottom-0 left-0 z-40 h-[calc(100dvh-72px)] w-screen"
+                                initial={{
+                                    backdropFilter: "blur(0px)",
+                                }}
+                                animate={{
+                                    backdropFilter: "blur(12px)",
+                                }}
+                                exit={{
+                                    backdropFilter: "blur(0px)",
+                                }}
+                                onClick={toggleMenu}
+                            />
+                            <motion.div
+                                className="fixed bottom-0 z-50 h-[calc(100dvh-72px)] w-[50vw] min-w-72 max-w-[100vw] bg-secondary"
+                                initial={{
+                                    right: "-100%",
+                                }}
+                                animate={{
+                                    right: isMenuOpen ? "0%" : "-100%",
+                                }}
+                                exit={{
+                                    right: "-100%",
+                                }}
+                            >
+                                <div className="flex h-full w-full">
+                                    <div className="h-full w-[2px] bg-gradient-to-b from-gradientPink to-gradientBlue" />
+                                    <div className="flex flex-col gap-8 p-8">
+                                        {navItems.map((item, index) => (
+                                            <NavItem key={index} {...item} />
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-                        </motion.div>
-                    </>
-                )}
-            </AnimatePresence>
-        </nav>
+                            </motion.div>
+                        </>
+                    )}
+                </AnimatePresence>
+            </nav>
+        </>
     );
 }
