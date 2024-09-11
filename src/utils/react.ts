b9222a05d216ee4fa390dabcf73e1b1b2b33e292
sandeyez <s.a.f.vanwerkhooven@students.uu.nl>
2024-09-11 19:08:24 +0200

    More cleverly disable scroll to maintain scrollbars and stop flickering

--- src/utils/react.ts
+++ src/utils/react.ts
@@ -1,7 +1,39 @@
 import { useContext } from "react";
 
+function preventDefault(event: Event) {
+    event.preventDefault();
+}
+
+function preventDefaultScrollKeys(event: KeyboardEvent) {
+    if (event.key === "ArrowUp" || event.key === "ArrowDown") {
+        event.preventDefault();
+    }
+}
+
+function disableScrolling() {
+    document.body.addEventListener("wheel", preventDefault, { passive: false });
+    document.body.addEventListener("touchmove", preventDefault, {
+        passive: false,
+    });
+    document.body.addEventListener("keydown", preventDefaultScrollKeys, {
+        passive: false,
+    });
+}
+
+function enableScrolling() {
+    document.body.removeEventListener("wheel", preventDefault);
+    document.body.removeEventListener("touchmove", preventDefault);
+    document.body.removeEventListener("keydown", preventDefaultScrollKeys);
+}
+
 export function toggleBodyScrolling(allowScrolling: boolean) {
-    document.body.classList.toggle("no-scroll", !allowScrolling);
+    if (allowScrolling) {
+        enableScrolling();
+        return;
+    }
+
+    disableScrolling();
+    // document.body.classList.toggle("no-scroll", !allowScrolling);
 }
 
 export function useContextSafely<T>(context: React.Context<T>) {
